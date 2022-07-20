b4dbab073f5f0b2b83eee2c38ae6ee94beeca259
jhslim <jlim.uw@gmail.com>
2022-07-19 21:55:02 -0700

    removed stub_view

--- blogging/urls.py
+++ blogging/urls.py
@@ -3,7 +3,6 @@ from blogging.views import list_view, detail_view
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    # path('', stub_view, name="blog_index"),
     path('', list_view, name="blog_index"),
     path('posts/<int:post_id>/', detail_view, name="blog_detail"),
     path('login', LoginView.as_view(template_name='login.html'), name='login'),
