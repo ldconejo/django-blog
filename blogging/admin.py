3ed08bc3ee5304408a35412b46b4bba03c65f33d
jhslim <jlim.uw@gmail.com>
2022-07-24 12:56:14 -0700

    minor changes

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,20 +1,23 @@
 from django.contrib import admin
-
-# Register your models here.
 from blogging.models import Post, Category
 
 
 class CategoryInPostInline(admin.TabularInline):
     model = Category.posts.through
+    extra = 2
 
 
 class PostAdmin(admin.ModelAdmin):
-    inlines = (CategoryInPostInline,)
+    inlines = [
+        CategoryInPostInline,
+    ]
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    inlines = (CategoryInPostInline,)
-    exclude = ('posts', )
+    inlines = [
+        CategoryInPostInline,
+    ]
+    exclude = ('posts',)
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category, CategoryAdmin)
